76639018b42705afebaa71aac1b81f1ada045066
Enes <enes.karatas@akinon.com>
2022-07-04 20:27:01 +0300

    flight and airport model created

--- flight/models.py
+++ flight/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 
+class Airport(models.Model):
+    code = models.CharField("Airport Code", unique=True, db_index=True, max_length=20)
+    name = models.CharField("Airport Name", max_length=150)
+
+
+class Flight(models.Model):
+    flight_number = models.CharField("Flight Number", max_length=20)
+    take_off = models.DateTimeField("Take Off Time")
+    landing = models.DateTimeField("Landing Time")
+    to_airport = models.ForeignKey(Airport, on_delete=models.ProtectedError,
+                                   related_name="to_airport")
+    from_airport = models.ForeignKey(Airport, on_delete=models.ProtectedError,
+                                     related_name="from_airport")
